5f07a64a7b13ea96501b23b23d2a27911931e5af
Hrishikesh Lamdade <hrishilamdade@gmail.com>
2022-03-19 08:55:11 +0530

    Login and register

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,6 +1,16 @@
 import imp
 from .base import *
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = 'info.user@realstate.com'
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Real State"
+
 DATABASES = {
     "default": {
         "ENGINE": env("POSTGRES_ENGINE"),
